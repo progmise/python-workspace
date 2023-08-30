5ea255568a9757bcd73cc51696b943cb1e560bac
progmise <58830364+progmise@users.noreply.github.com>
2023-08-30 01:56:06 +0000

    Un nuevo mensaje

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,6 @@
-print("Hola Mundo!")
+un_mensaje = "Esto es un código escrito desde VSC Web"
+
+numero = int(input("Ingrese un número: "))
+
+print(un_mensaje)
+print(f"Se ingreso el número {numero}")
